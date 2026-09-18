ac4eba7a4e77a5d7696946ee1c4f6d22af03f21d
AdamDXR <haritsaadam@gmail.com>
2026-09-18 16:44:30 +0700

    migration: create table genres and genre_movie

--- database/migrations/2026_09_16_165151_create_genres_table.php
+++ database/migrations/2026_09_16_165151_create_genres_table.php
@@ -11,17 +11,28 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Tabel utama untuk genres
         Schema::create('genres', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        // Tabel penghubung antara Movie dan Genre
+        Schema::create('genre_movie', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('movie_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('genre_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
-
+    
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
+        Schema::dropIfExists('genre_movie');
         Schema::dropIfExists('genres');
     }
 };
