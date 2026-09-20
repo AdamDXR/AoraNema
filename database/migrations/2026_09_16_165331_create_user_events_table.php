3707f99f1a43a4906aa1cc95517adf9220beca3c
AdamDXR <haritsaadam@gmail.com>
2026-09-20 20:58:43 +0700

    feat: create user_events table migration

--- database/migrations/2026_09_16_165331_create_user_events_table.php
+++ database/migrations/2026_09_16_165331_create_user_events_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('user_events', function (Blueprint $table) {
             $table->id();
+            
             $table->timestamps();
         });
     }
