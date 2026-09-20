e1673a48a598737520ccadbc6efc52a0f4f74f8e
AdamDXR <haritsaadam@gmail.com>
2026-09-20 21:01:01 +0700

    feat: create user_events table migration

--- database/migrations/2026_09_16_165331_create_user_events_table.php
+++ database/migrations/2026_09_16_165331_create_user_events_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('user_events', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('movie_id')->constrained()->cascadeOnDelete();
+            
+            // Jenis aktivitas: 'view' (melihat detail), 'click' (klik poster), 'search'
+            $table->string('event_type'); 
+            
+            // Nilai tambahan opsional (misal jika event_type = 'rate', maka event_value = '5')
+            $table->string('event_value')->nullable(); 
             
             $table->timestamps();
         });
