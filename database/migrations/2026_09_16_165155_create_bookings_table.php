f9435b6f05630f4515cca0ab719b8c3bdd3c9fbe
AdamDXR <haritsaadam@gmail.com>
2026-09-20 08:43:33 +0700

    feat: create bookings table migration

--- database/migrations/2026_09_16_165155_create_bookings_table.php
+++ database/migrations/2026_09_16_165155_create_bookings_table.php
@@ -13,7 +13,20 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+            // buat tau siapa yang pesan
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            // nonton jam berapanya
+            $table->foreignId('showtime_id')->constrained()->cascadeOnDelete();
+            // kursi mana yang dipesan
+            $table->foreignId('seat_id')->constrained()->cascadeOnDelete();
+
+            $table->integer('price');
+            $table->string('status')->default('pending'); 
+            //nanti ada pending, paid, dan cancelled
             $table->timestamps();
+
+            // biar nggak ada jam dan kursi yang ke double
+            $table->unique(['showtime_id', 'seat_id']);
         });
     }
 
