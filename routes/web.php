579beb42a10d89e5c16ac6c2dc781dd56786b921
Syakira Fara Salsabila <xxkirasya@gmail.com>
2026-09-21 11:09:42 +0700

    feat: beranda AoraNema dengan hero slider dan daftar film

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('beranda');
+});
+
+Route::get('/masuk', function () {
+    return view('masuk');
 });
