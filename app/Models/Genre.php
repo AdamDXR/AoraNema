1dce46593caae6665500546871e57718e7a2254d
AdamDXR <haritsaadam@gmail.com>
2026-09-21 13:03:43 +0700

    feat: create Genre model with many-to-many relationship to movies

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Genre extends Model
 {
-    //
+    // buat membuka izin pengisian data massal lagi dari API TMDB
+    protected $guarded = ['id'];
+
+    // relasi ke tabel movies (Many-to-Many)
+    public function movies(): BelongsToMany
+    {
+        return $this->belongsToMany(Movie::class);
+    }
+    
 }
